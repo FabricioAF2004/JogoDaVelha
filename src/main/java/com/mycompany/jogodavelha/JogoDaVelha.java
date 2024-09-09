ed38b46eb69de002d53709e2834089bab7c8cbf5
Fabricio <fabricioalvesfragoso@gmail.com>
2024-09-09 20:10:49 -0300

    Cronometro

---
+++ src/main/java/com/mycompany/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jogodavelha;
+
+/**
+ *
+ * @author Fabrício
+ */
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+        Tela form = new Tela();    
+      form.setVisible(true);
+    }
+}
